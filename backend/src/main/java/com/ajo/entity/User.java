f0c1c869e338b06cbc9999bec4fe5e08824dcf65
Simbiat23 <tsimbiat@yahoo.com>
2026-08-11 10:09:57 +0100

    Added all the getters and setter to the entity class

--- backend/src/main/java/com/ajo/entity/User.java
+++ backend/src/main/java/com/ajo/entity/User.java
@@ -18,19 +18,48 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private UUID id;
     private String email;
-    private String userName;
+    private String firstName;
+    private String lastName;
     private String password;
 
-    public User (String email, String userName, String password) {
+    public User (String email,String password, String firstName, String lastName) {
         this.email = email;
-        this.userName = userName;
+        this.lastName = lastName;
+        this.firstName = firstName;
         this.password = password;
 
         
     }
 
     public User() {
-        this(null, null, null);
+        this(null, null, null, null);
     }
+
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     
 }
