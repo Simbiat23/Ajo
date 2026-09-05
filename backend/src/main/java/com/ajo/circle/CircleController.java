c28d69bf4850f5fee02a5c9315fc8dcd47d61b3c
Simbiat23 <tsimbiat@yahoo.com>
2026-09-05 17:45:22 +0100

    Add error handling and display for circle form validation and submission

--- backend/src/main/java/com/ajo/circle/CircleController.java
+++ backend/src/main/java/com/ajo/circle/CircleController.java
@@ -2,6 +2,7 @@ package com.ajo.circle;
 
 import java.util.List;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:5174"})
 @RestController
 @RequestMapping("/ajo/circle")
 
