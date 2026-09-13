a1a84f47a029a24accc4293adf7bad9e0759e471
Simbiat23 <tsimbiat@yahoo.com>
2026-09-13 12:14:07 +0100

    Add Swagger/OpenAPI documentation with springdoc 3.1.1

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -3,7 +3,7 @@ INSERT IGNORE INTO users (email, first_name, last_name, password) VALUES ('davit
 INSERT IGNORE INTO users (email, first_name, last_name, password) VALUES ('tsimbiat@yahoo.com','Simbiat', 'Taofeek', 'jade4477' );
 INSERT IGNORE INTO users (email, first_name, last_name, password) VALUES ('tayeTaofeek@yahoo.com','Jumoke', 'Taofeek', 'england208' );
 
--- SEEded data for circle
+-- seed data for circle
 INSERT IGNORE INTO circles (name, contribution_amount, frequency, max_members, start_date, invite_code, current_cycle, organiser_id) VALUES('FamilyCircle', 100,'BIWEEKLY', 5, '2026-09-12', 'AJO-0966', 1, NULL);
 INSERT IGNORE INTO circles (name, contribution_amount, frequency, max_members, start_date, invite_code, current_cycle, organiser_id) VALUES('FriendsCircle', 50, 'WEEKLY', 8, '2026-09-15', 'AJO-1142', 1, NULL);
 INSERT IGNORE INTO circles (name, contribution_amount, frequency, max_members, start_date, invite_code, current_cycle, organiser_id) VALUES('WorkCircle', 250, 'MONTHLY', 6, '2026-10-01', 'AJO-2384', 1, NULL);
