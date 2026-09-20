f0097714696307aa51d064c01e76a60065e83a66
Simbiat23 <tsimbiat@yahoo.com>
2026-09-20 15:22:29 +0100

    Add form validation check for invitecode

--- frontend/src/components/JoinCircleForm.tsx
+++ frontend/src/components/JoinCircleForm.tsx
@@ -1,5 +1,5 @@
 import { httpApi } from "@/api/api"
-import type { CircleResponse, UserResponse } from "@/types/types"
+import type { CircleResponse,UserResponse } from "@/types/types"
 import { Box, Button, Field, Heading, Input, Stack, Text } from "@chakra-ui/react"
 import {useState } from "react"
 
@@ -10,13 +10,18 @@ interface JoinCircleFormProps {
 }
 
 
-
 export default function JoinCircleForm({onJoinCircle, loggedInUser}: JoinCircleFormProps) {
     const [inviteCode, setInviteCode] = useState('')
+    const [errors, setErrors] = useState('')
     const [submitError, setSubmitError] = useState('')
 
     async function handleSubmit(event:React.SubmitEvent<HTMLDivElement>) {
         event.preventDefault()
+        const isValid = /^AJO-\d{4}$/.test(inviteCode)
+        if (!isValid) {
+            setErrors('Please enter a valid invite code')
+            return;
+        }
         try {
             const joinCircle = await httpApi.joinCircle(inviteCode, loggedInUser.id)
             onJoinCircle(joinCircle);
@@ -33,9 +38,10 @@ export default function JoinCircleForm({onJoinCircle, loggedInUser}: JoinCircleF
         <Box  as="form" onSubmit={handleSubmit} borderWidth= "1px" borderRadius="md" padding="6" maxWidth="480px">
             <Heading size="md" marginBottom="4">Join A Circle</Heading>
             <Stack gap={4}>
-                <Field.Root>
+                <Field.Root invalid={!!errors}>
                     <Field.Label>Invite Code</Field.Label>         
                     <Input value={inviteCode} onChange={(event) => setInviteCode(event.target.value)} placeholder="Enter invite code" type="text" />
+                    {errors && <Field.ErrorText>{errors}</Field.ErrorText>}
                 </Field.Root>
                 </Stack>
                 
